9c2a71bfdc5b7cbba969308e9e7f269a88fa64ec
segoldsmith <steven.e.goldsmith@gmail.com>
2024-08-05 19:19:32 -0400

    2nd commit - fixing typos

--- SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -12,9 +12,7 @@ You will complete questions 1-9 below in the PHPMyAdmin interface.
 Log in by pasting the following URL into your browser, and
 using the following Username and Password:
 
-URL: https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
+
 
 The data you need is in the "country_club" database. This database
 contains 3 tables:
